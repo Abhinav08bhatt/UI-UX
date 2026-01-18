09b939da5204d7b819e415b50949487d8a91692e
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2026-01-18 16:46:53 +0530

    fixed appbar

--- lib/WhatsApp/whatsapp_home.dart
+++ lib/WhatsApp/whatsapp_home.dart
@@ -24,51 +24,154 @@ class WhatsappHome extends StatelessWidget{
     return Scaffold(
         
         backgroundColor: whatsappWhite,
-      
-      
+
+
         appBar: AppBar(
           automaticallyImplyLeading: false,
           backgroundColor: whatsappWhite,
-          toolbarHeight: 50,
+          toolbarHeight: 126,
+          surfaceTintColor: whatsappWhite,
           
-          title: Text("WhatsApp",
-            style: GoogleFonts.inter(
-              fontSize: 34,
-              height: 1,
-              letterSpacing: -1,
-              color:primaryColor,
-              fontWeight: FontWeight.w600,
-            ),
+          title: Column(
+            children: [
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  Text("WhatsApp",
+                    style: GoogleFonts.inter(
+                      fontSize: 34,
+                      height: 1,
+                      letterSpacing: -1,
+                      color:primaryColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+          
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8,2,8,0),
+                        child: GestureDetector(
+                          child: FaIcon(
+                            FontAwesomeIcons.camera,
+                            color: Colors.black87,
+                            size: 22,
+                          ),
+                        ),
+                      ),
+            
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(12,3,0,0),
+                        child: GestureDetector(
+                          child: FaIcon(
+                            Icons.more_vert_rounded,
+                            color: Colors.black87,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),  
+
+                ],
+              ),
+                
+              SizedBox(height: 12,),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,8,0,8),
+                child: Row(
+                  children: [
+                          
+                    Expanded(
+                      child: TextField(
+                      
+                        style: const TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16,
+                        ),
+
+                        cursorColor:  primaryColor,
+                      
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: whatsappWhite,
+                          contentPadding: EdgeInsets.fromLTRB(20, 16, 20, 16),
+
+                          hint: Text(
+                            "Search for contact or file",
+                            style: text.copyWith(fontSize: 16)
+                          ),
+                      
+                          label: Text(
+                            "Search",
+                            style: text.copyWith(fontSize: 16),
+                          ),
+                      
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32),
+                            borderSide: BorderSide(
+                              color:  Colors.black26,
+                              width: 1.5,
+                            )
+                          ),
+                      
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32),
+                            borderSide: BorderSide(
+                              color: primaryColor,
+                              width: 1.5,
+                            )
+                          ),
+                      
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.fromLTRB(10,10,2,10),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black54,
+                              size:28,
+                            ),
+                          ),
+                      
+                        ),
+                          
+                      ),
+                    ),
+                          
+                    SizedBox(width: 8,),
+                
+                    Container(
+                      height: 54,
+                      width: 54,
+                      decoration: BoxDecoration(
+                        color: primaryColor,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: IconButton(
+          
+                        onPressed: (){}, 
+                        
+                        icon: FaIcon(
+                          FontAwesomeIcons.userPlus,
+                          color: whatsappWhite,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+          
+                  ],
+                ),
+              ),
+
+            ],
           ),
       
-          actions: [
-            
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8,2,8,0),
-              child: GestureDetector(
-                child: FaIcon(
-                  FontAwesomeIcons.camera,
-                  color: Colors.black87,
-                  size: 22,
-                ),
-              ),
-            ),  
-      
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12,3,10,0),
-              child: GestureDetector(
-                child: FaIcon(
-                  Icons.more_vert_rounded,
-                  color: Colors.black87,
-                  size: 25,
-                ),
-              ),
-            ),
-      
-          ],
-      
         ),
-      
+
+
         body: SafeArea(
           child: GestureDetector(
             behavior: HitTestBehavior.translucent,
@@ -79,100 +182,18 @@ class WhatsappHome extends StatelessWidget{
               keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
               children: [
             
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(12,8,12,8),
-                  child: Row(
-                    children: [
-                            
-                      Expanded(
-                        child: TextField(
-                        
-                          style: const TextStyle(
-                            color: Colors.black87,
-                            fontSize: 20,
-                          ),
-
-                          cursorColor:  primaryColor,
-                        
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: whatsappWhite,
-                            contentPadding: EdgeInsets.fromLTRB(20, 16, 20, 16),
-
-                            hint: Text(
-                              "Search for contact or file",
-                              style: text.copyWith(fontSize: 16)
-                            ),
-                        
-                            label: Text(
-                              "Search",
-                              style: text.copyWith(fontSize: 16),
-                            ),
-                        
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32),
-                              borderSide: BorderSide(
-                                color:  Colors.black26,
-                                width: 1.5,
-                              )
-                            ),
-                        
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32),
-                              borderSide: BorderSide(
-                                color: primaryColor,
-                                width: 1.5,
-                              )
-                            ),
-                        
-                            prefixIcon: Padding(
-                              padding: const EdgeInsets.fromLTRB(10,10,2,10),
-                              child: Icon(
-                                Icons.search,
-                                color: Colors.black54,
-                                size:28,
-                              ),
-                            ),
-                        
-                          ),
-                            
-                        ),
-                      ),
-                            
-                      SizedBox(width: 8,),
-                  
-                      Container(
-                        height: 56,
-                        width: 56,
-                        decoration: BoxDecoration(
-                          color: primaryColor,
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: IconButton(
-            
-                          onPressed: (){}, 
-                          
-                          icon: FaIcon(
-                            FontAwesomeIcons.userPlus,
-                            color: whatsappWhite,
-                            size: 18,
-                          ),
-                        ),
-                      ),
-            
-                    ],
-                  ),
-                ),
                   
                 // contacts here
 
                 ContactCard(
                   profilePic: AssetImage("assets/images/profile_pic_1.jpg"),
                   name: "Avi", 
+                  messageCase : "receiving",
                   messageStatus: true,
                   messageRead: true,
                   message: "This Is working and it is just amazing that how better it looks", 
                   time: "00:00 am",
+                  messageCount: 1,
                   nameStyle: name,
                   textStyle: text,
                 ),
@@ -180,21 +201,25 @@ class WhatsappHome extends StatelessWidget{
                 ContactCard(
                   profilePic: null,
                   name: "Avi", 
+                  messageCase : "receiving",
                   messageStatus: true,
                   messageRead: true,
                   message: "This Is working and it is just amazing that how better it looks", 
                   time: "00:00 am",
+                  messageCount: 2,
                   nameStyle: name,
                   textStyle: text,
                 ),
 
                 ContactCard(
                   profilePic: AssetImage("assets/images/profile_pic_3.jpg"),
-                  name: "Avi", 
+                  name: "Liam", 
+                  messageCase : "sending",
                   messageStatus: true,
                   messageRead: true,
-                  message: "This Is working and it is just amazing that how better it looks", 
+                  message: "If you could teleport anywhere right now, where would you go?", 
                   time: "00:00 am",
+                  messageCount: 0,
                   nameStyle: name,
                   textStyle: text,
                 ),
@@ -202,21 +227,25 @@ class WhatsappHome extends StatelessWidget{
                 ContactCard(
                   profilePic: AssetImage("assets/images/profile_pic_4.jpg"),
                   name: "Avi", 
+                  messageCase : "sending",
                   messageStatus: true,
                   messageRead: true,
                   message: "This Is working and it is just amazing that how better it looks", 
                   time: "00:00 am",
+                  messageCount: 0,
                   nameStyle: name,
                   textStyle: text,
                 ),
 
                 ContactCard(
                   profilePic: AssetImage("assets/images/profile_pic_2.jpg"),
-                  name: "Avi", 
+                  name: "Maya", 
+                  messageCase : "receiving",
                   messageStatus: true,
                   messageRead: true,
-                  message: "This Is working and it is just amazing that how better it looks", 
+                  message: "I just realized coffee tastes better when it's raining.", 
                   time: "00:00 am",
+                  messageCount: 4,
                   nameStyle: name,
                   textStyle: text,
                 ),
@@ -224,17 +253,23 @@ class WhatsappHome extends StatelessWidget{
                 ContactCard(
                   profilePic: AssetImage("assets/images/profile_pic_5.jpg"),
                   name: "Avi", 
-                  messageStatus: true,
-                  messageRead: true,
+                  messageCase : "sending",
+                  messageStatus: false,
+                  messageRead: false,
                   message: "This Is working and it is just amazing that how better it looks", 
                   time: "00:00 am",
+                  messageCount: 1,
                   nameStyle: name,
                   textStyle: text,
                 ),
 
                 Spacer(),
             
-                // Divider(),
+                Divider(
+                  indent: 8,
+                  endIndent: 8,
+                  color: whatsappBlack.withValues(alpha: 0.10),
+                ),
             
                 ElevatedButton(
                   onPressed: () {
@@ -257,13 +292,15 @@ class WhatsappHome extends StatelessWidget{
                   child: Row(
                     children: [
                   
+                      SizedBox(width: 16,),
+                  
                       FaIcon(
                         Icons.archive_outlined,
                         color: whatsappBlack.withValues(alpha: 0.80),
                         size: 20,
                       ),
                   
-                      SizedBox(width: 14,),
+                      SizedBox(width: 28,),
                   
                       Text(
                         "Archived",
@@ -279,6 +316,7 @@ class WhatsappHome extends StatelessWidget{
           )
         ),
       
+
     );
   }
 }
@@ -288,15 +326,19 @@ class ContactCard extends StatelessWidget{
   final String name , message , time;
   final TextStyle nameStyle;
   final TextStyle textStyle;
-  final bool messageStatus ,messageRead;
+  final String messageCase;
+  final bool messageStatus,messageRead;
+  final int messageCount;
 
   const ContactCard({
     super.key,
     this.profilePic,
     required this.name,
+    required this.messageCase,
     required this.messageStatus,
     required this.messageRead,
     required this.message,
+    required this.messageCount,
     required this.time,
     required this.nameStyle,
     required this.textStyle,
@@ -330,7 +372,7 @@ class ContactCard extends StatelessWidget{
         backgroundImage : profilePic,
         
         child: profilePic == null
-          ? Icon(Icons.person,color: Colors.black54,)
+          ? Icon(Icons.person,color: secondaryColor,)
           : null,
       ),
 
@@ -339,13 +381,41 @@ class ContactCard extends StatelessWidget{
       subtitle: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
+          
+          if (messageCase == "sending")
+            Icon(
+              messageStatus
+                  ? Icons.done_all_rounded
+                  : Icons.done_rounded,
+              size: 20,
+              color: messageRead
+                  ? Colors.lightBlue
+                  : Colors.black54,
+            )
+          else
+            Stack(
+              children: [
 
-          Icon(
-            messageStatus ? Icons.done_all_rounded : Icons.done_rounded,
-            size: 20,
-            color : messageRead ? Colors.lightBlue : Colors.black54,
-          ),
+              Icon(
+                Icons.circle,
+                color: primaryColor,
+                size: 23,
+              ),
 
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.5, 4, 8, 4),
+                child: Text(
+                  "$messageCount",
+                  style: TextStyle(
+                    color: whatsappWhite,
+                    fontSize: 13
+                  ),
+                ),
+              )
+
+              ],
+            ),
+          
           SizedBox(width: 4,),
 
           Expanded(
@@ -370,4 +440,36 @@ class ContactCard extends StatelessWidget{
 
     );
   }
+}
+
+enum MessageType{
+  sending,
+  receiving
+}
+
+class ChatPreview {
+  final ImageProvider? profilePic;
+  final String name;
+  final String message;
+  final String time;
+  final MessageType type;
+
+  final bool? sent;
+  final bool? read;
+
+  final int? unreadCount;
+
+  ChatPreview({
+    this.profilePic,
+    required this.name,
+    required this.message,
+    required this.time,
+    required this.type,
+
+    this.sent = false,
+    this.read = false,
+    this.unreadCount = 0,
+    
+  });
+
 }
